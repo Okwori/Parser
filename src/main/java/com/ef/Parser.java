fddb28fec2d7ad0ce63a5ecea15937aa7dbcc8f6
Simon Okwori <simon.okwori@gmail.com>
2019-06-13 08:51:31 +0200

    initial

---
+++ src/main/java/com/ef/Parser.java
@@ -0,0 +1,19 @@
+package com.ef;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parser implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parser.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
