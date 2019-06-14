00e382dd2df76865ef3ec4a4bb4b947818062041
Simon Okwori <simon.okwori@gmail.com>
2019-06-14 22:45:38 +0200

    main class updated

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -1,12 +1,24 @@
 package com.ef;
 
+import com.ef.model.AccessLog;
+import com.ef.model.BlockedIp;
+import com.ef.service.AccessLogService;
+import com.ef.service.ApplicationArgumentParameter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
 public class Parser implements ApplicationRunner {
+
+    @Autowired
+    AccessLogService logService;
 
     public static void main(String[] args) {
         SpringApplication.run(Parser.class, args);
@@ -14,6 +26,25 @@ public class Parser implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Application Started!");
 
+        ApplicationArgumentParameter parameter = new ApplicationArgumentParameter(args);
+
+        if (!parameter.checkArguments()) {
+            System.out.println("Help: java -jar parser-0.0.1.jar --accesslog=./access.log --startDate=2017-01-01.00:00:00 --duration=daily --threshold=500");
+        } else {
+            if (parameter.isErrorFlag()) {
+                log.error("JobExecution: FAILED! -- Restart Application with the right parameters");
+            } else {
+                System.out.println("JobExecution: " + logService.load());
+                List<AccessLog> logs = logService.getLogsByDateDurationAndLimit(parameter.getStartDate(), parameter.getEndDate(), parameter.getThreshold());
+
+                List<BlockedIp> blockedIpList = logService.saveBlockedIps(logs, parameter);
+                blockedIpList.forEach(blockedIp -> System.out.println(blockedIp.getComment()));
+
+                System.out.println(logs.size() == 1 | logs.size() == 0 ? "Found " + logs.size() + " IP Address" : "Found " + logs.size() + " IP Addresses");
+                System.out.println("Success!!");
+            }
+        }
     }
 }
