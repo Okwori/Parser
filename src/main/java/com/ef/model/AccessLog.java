e8b46e2f3b6dab49ff7926f40676e73dc65440ee
Simon Okwori <simon.okwori@gmail.com>
2019-06-14 10:16:35 +0200

    updated entity classes

--- src/main/java/com/ef/model/AccessLog.java
+++ src/main/java/com/ef/model/AccessLog.java
@@ -1,18 +1,26 @@
 package com.ef.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.io.Serializable;
 import java.util.Date;
 
-@Entity
+@Entity @IdClass(AccessLog.AccessLogID.class)
 @Getter @Setter @NoArgsConstructor
 public class AccessLog {
 
+    @Id
+    @Column(columnDefinition = "DATETIME(3)")
     private Date date;
 
+    @Id
     private String ipAddress;
 
     private String request;
@@ -29,5 +37,11 @@ public class AccessLog {
                 ", status=" + status +
                 ", userAgent=" + userAgent +
                 '}';
+    }
+
+    @Data
+    static class AccessLogID implements Serializable {
+        Date date;
+        String ipAddress;
     }
 }
--- src/main/java/com/ef/model/BlockedIp.java
+++ src/main/java/com/ef/model/BlockedIp.java
@@ -3,11 +3,13 @@ package com.ef.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class BlockedIp {
 
+    @Id
     private String ipAddress;
 
     private String comment;
